d170a21fedf2c6d0ebbde04fda134fc25453e071
Nicolly <saantos.nick1606@gmail.com>
2026-02-02 08:55:08 -0300

    feat: finaliza atividade de lidando com objetos

---
+++ ex_01.js
@@ -0,0 +1,6 @@
+const post = {titulo: "Introdução JS"};
+const copiaPOst = post;
+
+copiaPOst.titulo = "Aprendendo React";
+
+console.log(post.titulo);
---
+++ ex_02.js
@@ -0,0 +1,7 @@
+const nome = "Nicolly Rodrigues de Souza Santos";
+const curso = {
+  nomedoCurso: "Desenvolvimento de Sistemas",
+  duracao: "72hr"
+};
+
+console.log(`Olá ${nome}, bem-vindo ao curso ${curso.nomedoCurso}`);
