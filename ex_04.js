4d9d00bcbb210d5806adcc2e2bc81ed41b7ac11f
Nicolly <saantos.nick1606@gmail.com>
2026-02-02 09:38:50 -0300

    feat: altera propriedade do objeto

---
+++ ex_04.js
@@ -0,0 +1,10 @@
+const nome = "Nicolly Rodrigues de Souza Santos";
+
+const curso = {
+  nomedoCurso: "Desenvolvimento de Sistemas",
+  duracao: "72hr"
+};
+
+curso.nomedoCurso = "Eletro";
+
+console.log(`Olá ${nome}, bem-vindo ao curso ${curso.nomedoCurso}`);
